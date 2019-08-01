837ade4627de0f9243e241fd5d9e997236764c46
jhmatthews <jm8g08@soton.ac.uk>
2019-08-01 14:53:40 +0100

    mods for elements

--- jet.py
+++ jet.py
@@ -16,7 +16,7 @@ kappa,theta,lnmu,lnsig,weight = 5.67, 5.96, 2.14, 0.31,0.82
 # let's do everything in units of kyr
 # run for 100 Myr (1e5 kyr) in bins of 100 kyr
 lognorm_params = (1.5,0,np.exp(1.5))
-RedNoiseL,RandomSeed,aliasTbin, tbin, Age = 100,12,100,100,1000
+RedNoiseL,RandomSeed,aliasTbin, tbin, Age = 100,12,100,100,100
 N = Age / tbin
 
 #delc = Simulate_DE_Lightcurve(BendingPL, (A,v_bend,a_low,a_high,c),
@@ -64,10 +64,13 @@ for i, energy in enumerate(energies):
 BETA = 2
 ncr = np.zeros_like(energies)
 escape_time = (1e19 / energies) * 1e7 * YR 
+frac_elem = [1.0,0.1,1e-4,3.16e-05]
+z_elem = [1,2,7,26]
 #flux[lc.time <= 10000] = 1e45
 #flux[lc.time > 10000] = 1e30
 
 for i in range(len(flux)):
+	#for j in range(len(f_elem)):
 	Emax = max_energy(flux[i], v_over_c=0.5)
 
 	print (Emax)
@@ -91,6 +94,7 @@ for i in range(len(flux)):
 	ncr += change
 	ncr[(ncr<0)] = 0
 
+	#if plot_all:
 	plt.clf()
 	plt.figure()
 	plt.subplot(211)
@@ -104,16 +108,20 @@ for i in range(len(flux)):
 	#plt.ylabel("n(E)")
 
 	#lt.plot(energies, ncr)
-	plt.plot(energies, energies*energies*ncr)
+	for j,frac in enumerate(frac_elem):
+		energy = energies * z_elem[j]
+		plt.plot(energy, energy*energy*(frac*ncr))
 	plt.loglog()
-	plt.xlim(1e15,1e21)
+	plt.xlim(1e17,1e21)
 	#plt.ylim(1e13,1e19)
 
 	plt.subplot(224)
-	plt.plot(energies, energies*energies*escaping*delta_t)
+	for j,frac in enumerate(frac_elem):
+		energy = energies * z_elem[j]
+		plt.plot(energy, energy*energy*frac*escaping*delta_t)
 
 	# plt.plot(energies, 1e15*((energies / 1e9)**-2), c="k", ls="--")
-	plt.xlim(1e15,1e21)
+	plt.xlim(1e17,1e21)
 	#plt.ylim(0.1,1e5)
 	#plt.plot(energies, 1e43*((energies / 1e9)**-3), c="k", ls=":")
 	plt.loglog()
